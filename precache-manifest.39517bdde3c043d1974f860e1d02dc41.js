2305e01dd393daaf055013924f0b1b278fb4558e
Dell <Dell@DESKTOP-MOA3QPD>
2020-05-28 23:20:56 +0530

    Updates

---
+++ precache-manifest.39517bdde3c043d1974f860e1d02dc41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "067050c2c464c89aa0bb3eec00af1bb6",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "e132592c681bd6936dc2",
+    "url": "/weather/static/css/main.bd498a73.chunk.css"
+  },
+  {
+    "revision": "394ed6084b70c0885b03",
+    "url": "/weather/static/js/2.217bc631.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.217bc631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e132592c681bd6936dc2",
+    "url": "/weather/static/js/main.0b0e575c.chunk.js"
+  },
+  {
+    "revision": "b83e05f3b1e38480a3ef",
+    "url": "/weather/static/js/runtime-main.d16f45af.js"
+  }
+]);
